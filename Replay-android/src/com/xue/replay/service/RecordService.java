fd05416597280779b9ff346424ab516d7898c1ae
wei.xue <wei.xue1210@gmail.com>
2013-12-30 23:30:41 +0800

    modify recordservice

--- Replay-android/src/com/xue/replay/service/RecordService.java
+++ Replay-android/src/com/xue/replay/service/RecordService.java
@@ -1,5 +1,15 @@
 package com.xue.replay.service;
 
-public class RecordService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class RecordService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
